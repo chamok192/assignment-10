eb7d34a8ad1b33f5c526513e902bca1c7dba5d3c
Chamok Bhadra <chamokbhadra@gmail.com>
2025-11-18 23:55:55 +0600

    all done

--- client/PlateShare/src/Pages/AvailableFoods.jsx
+++ client/PlateShare/src/Pages/AvailableFoods.jsx
@@ -43,7 +43,7 @@ const AvailableFoods = () => {
             </div>
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {data
 
               .filter((foods) => foods.food_status === "Available")
